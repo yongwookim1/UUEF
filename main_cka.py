3adf2692410e63b099b07ab7c8c2825e64cc6546
yongwookim1 <yongwookim1@korea.ac.kr>
2024-12-02 16:25:55 +0900

    fix: modify main_cka.py

--- main_cka.py
+++ main_cka.py
@@ -1,28 +1,27 @@
-from collections import OrderedDict
-
 import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
-from torchvision import models
-from torchvision import transforms
+from torchvision import models, transforms
 from tqdm import tqdm
 import os
-import random
 from PIL import Image
 
 import arg_parser
 import utils
 from CKA.CKA import CudaCKA
 
+# configuration
 args = arg_parser.parse_args()
-
-seed = 2
-utils.setup_seed(seed)
-
+utils.setup_seed(2)
 device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
+data_dir = '/home/dataset/OfficeHomeDataset_10072016/Real World' # set your dataset path
 
-data_dir = '/home/dataset/OfficeHomeDataset_10072016/Real World'
+# model paths
+pretrained_model_paths = [
+    "./pretrained_model/0model_SA_best159.pth.tar", # set your model path
+    "./pretrained_model/retraincheckpoint100.pth.tar",
+]
 
 
 class OfficeHomeDataset(Dataset):
@@ -56,27 +55,6 @@ class OfficeHomeDataset(Dataset):
         return image, label
 
 
-data_transforms = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406],
-                         [0.229, 0.224, 0.225])
-])
-
-# craete dataset
-full_dataset = OfficeHomeDataset(data_dir, transform=data_transforms)
-
-data_loader = DataLoader(full_dataset, batch_size=1024, shuffle=False, num_workers=4)
-
-# model paths
-pretrained_model_paths = [
-    # add paths if you need
-    "./pretrained_model/retraincheckpoint100.pth.tar",
-    "./pretrained_model/0model_SA_best159.pth.tar",
-]
-
-
 def load_model(pretrained_model_path, device):
     print(f"\nLoading model: {pretrained_model_path}")
     model = models.resnet50(weights=None)
@@ -90,24 +68,19 @@ def load_model(pretrained_model_path, device):
     return model
 
 
-def hook_fn_o(module, input, output):
-    global features_o
-    features_o.append(output)
-
-
-def hook_fn_r(module, input, output):
-    global features_r
-    features_r.append(output)
+class FeatureExtractor:
+    def __init__(self):
+        self.features = []
+        
+    def hook_fn(self, module, input, output):
+        self.features.append(output)
+        
+    def clear(self):
+        self.features = []
 
 
 def main():
-    args = arg_parser.parse_args()
-    seed = 2
-    utils.setup_seed(seed)
-
-    device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
-
-    data_dir = '/home/dataset/OfficeHomeDataset_10072016/Real World'
+    # data setup
     data_transforms = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
@@ -118,56 +91,53 @@ def main():
     full_dataset = OfficeHomeDataset(data_dir, transform=data_transforms)
     data_loader = DataLoader(full_dataset, batch_size=1024, shuffle=False, num_workers=4)
 
-    pretrained_model_paths = [
-        "./pretrained_model/0model_SA_best159.pth.tar",
-        "./pretrained_model/retraincheckpoint100.pth.tar",
-    ]
-
+    # model setup
     model = load_model(pretrained_model_paths[1], device).to(device)
     retrained_model = load_model(pretrained_model_paths[0], device).to(device)
-
     model.eval()
     retrained_model.eval()
 
-    global features_o, features_r
+    # feature extractors
+    original_extractor = FeatureExtractor()
+    retrained_extractor = FeatureExtractor()
+    
     linear_cka = kernel_cka = linear_check = kernel_check = 0
+    cuda_cka = CudaCKA(device)
 
-    for i, data in enumerate(tqdm(data_loader)):
+    for data in tqdm(data_loader):
         img, _ = data
         img = img.to(device)
 
-        features_o = []
-        features_r = []
-
-        hooks_o = []
-        hooks_r = []
-
-        hooks_o.append(model.avgpool.register_forward_hook(hook_fn_o))
-        hooks_r.append(retrained_model.avgpool.register_forward_hook(hook_fn_r))
+        # register hooks
+        hooks = [
+            model.avgpool.register_forward_hook(original_extractor.hook_fn),
+            retrained_model.avgpool.register_forward_hook(retrained_extractor.hook_fn)
+        ]
 
         with torch.no_grad():
-            original_output = model(img)
-            retrained_output = retrained_model(img)
+            model(img)
+            retrained_model(img)
 
-            f_o, f_r = features_o[0], features_r[0]
-            f_o = f_o.view(f_o.size(0), -1)
-            f_r = f_r.view(f_r.size(0), -1)
+            f_o = original_extractor.features[0].view(original_extractor.features[0].size(0), -1)
+            f_r = retrained_extractor.features[0].view(retrained_extractor.features[0].size(0), -1)
 
-            cuda_cka = CudaCKA(device)
             linear_cka += cuda_cka.linear_CKA(f_o, f_r)
             linear_check += cuda_cka.linear_CKA(f_o, f_o)
             kernel_cka += cuda_cka.kernel_CKA(f_o, f_r)
             kernel_check += cuda_cka.kernel_CKA(f_r, f_r)
 
-        for hook in hooks_o:
+        # cleanup
+        for hook in hooks:
             hook.remove()
-        for hook in hooks_r:
-            hook.remove()
+        original_extractor.clear()
+        retrained_extractor.clear()
 
-    print(f"Linear CKA: {linear_cka / len(data_loader):.3f}")
-    print(f"Kernel CKA: {kernel_cka / len(data_loader):.3f}")
-    print(f"Linear CKA check: {linear_check / len(data_loader):.3f}")
-    print(f"Kernel CKA check: {kernel_check / len(data_loader):.3f}")
+    # print results
+    n = len(data_loader)
+    print(f"Linear CKA: {linear_cka / n:.3f}")
+    print(f"Kernel CKA: {kernel_cka / n:.3f}")
+    print(f"Linear CKA check: {linear_check / n:.3f}")
+    print(f"Kernel CKA check: {kernel_check / n:.3f}")
 
 
 if __name__ == "__main__":
