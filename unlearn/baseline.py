f5c440a7510bc28a8e6223f1cf4b4519f5165d46
yongwookim1 <yongwookim1@korea.ac.kr>
2024-12-02 16:30:40 +0900

    fix: modify baseline.py

--- unlearn/baseline.py
+++ unlearn/baseline.py
@@ -12,13 +12,6 @@ sys.path.append(".")
 from imagenet import get_x_y_from_data_dict
 
 
-def l1_regularization(model):
-    params_vec = []
-    for param in model.parameters():
-        params_vec.append(param.view(-1))
-    return torch.linalg.norm(torch.cat(params_vec), ord=1)
-
-
 @iterative_unlearn
 def GAwithKD(data_loaders, model, criterion, optimizer, epoch, args, mask=None):
     # Store initial model state at the beginning of unlearning (epoch 0)
@@ -79,7 +72,7 @@ def GAwithKD(data_loaders, model, criterion, optimizer, epoch, args, mask=None):
                     )
                 )
                 start = time.time()
-        # restore phase
+
         print("Restore phase")
         for i, data in enumerate(distill_loader):
             image, target = get_x_y_from_data_dict(data, device)
@@ -89,10 +82,14 @@ def GAwithKD(data_loaders, model, criterion, optimizer, epoch, args, mask=None):
             with torch.no_grad():
                 output_o = original_model(image)
             
-            loss = F.kl_div(F.log_softmax(output_u, dim=1), F.softmax(output_o, dim=1), reduction='batchmean')
+            # combine KD loss and CE loss
+            kd_loss = F.kl_div(F.log_softmax(output_u, dim=1), F.softmax(output_o, dim=1), reduction='batchmean')
+            ce_loss = criterion(output_u, target)
+            
+            loss = 10 * kd_loss + 10 * ce_loss
+            
             optimizer.zero_grad()
             loss.backward()
-
             optimizer.step()
 
             output = output_u.float()
@@ -110,43 +107,6 @@ def GAwithKD(data_loaders, model, criterion, optimizer, epoch, args, mask=None):
                     "Accuracy {top1.val:.3f} ({top1.avg:.3f})\t"
                     "Time {3:.2f}".format(
                         epoch, i, len(distill_loader), end - start, loss=losses, top1=top1
-                    )
-                )
-                start = time.time()
-    else:
-        for i, (image, target) in enumerate(forget_loader):
-            if epoch < args.warmup:
-                utils.warmup_lr(
-                    epoch, i + 1, optimizer, one_epoch_step=len(forget_loader), args=args
-                )
-
-            image = image.to(device)
-            target = target.to(device)
-
-            # compute output
-            output_clean = model(image)
-            loss = -criterion(output_clean, target)
-
-            optimizer.zero_grad()
-            loss.backward()
-
-            optimizer.step()
-
-            output = output_clean.float()
-            loss = loss.float()
-            prec1 = utils.accuracy(output.data, target)[0]
-
-            losses.update(loss.item(), image.size(0))
-            top1.update(prec1.item(), image.size(0))
-
-            if (i + 1) % args.print_freq == 0:
-                end = time.time()
-                print(
-                    "Epoch: [{0}][{1}/{2}]\t"
-                    "Loss {loss.val:.4f} ({loss.avg:.4f})\t"
-                    "Accuracy {top1.val:.3f} ({top1.avg:.3f})\t"
-                    "Time {3:.2f}".format(
-                        epoch, i, len(forget_loader), end - start, loss=losses, top1=top1
                     )
                 )
                 start = time.time()
