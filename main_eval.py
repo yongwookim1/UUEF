95f23441a7dbb51d2e41b90755ba3c67c9105c29
yongwookim1 <yongwookim1@korea.ac.kr>
2025-02-05 12:10:00 +0900

    fix: fix error

--- main_eval.py
+++ main_eval.py
@@ -71,6 +71,7 @@ def evaluate_model(model_path, retrained_model_path, device, args):
     
     results.update({
         'imagenet_forget_knn': knn_accuracy['imagenet_forget'],
+        'imagenet_retain_knn': knn_accuracy['imagenet_retain'],
         'imagenet_val_forget_knn': knn_accuracy['imagenet_val_forget'],
         'imagenet_val_retain_knn': knn_accuracy['imagenet_val_retain'],
         'office_home_real_world_knn': knn_accuracy['office_home_real_world'],
@@ -88,10 +89,10 @@ def evaluate_model(model_path, retrained_model_path, device, args):
     print("Evaluating CKA...")
     model = utils.initialize_model(model_path, device, arch=args.arch)
     retrained_model = utils.initialize_model(retrained_model_path, device, arch=args.arch)
-    forget_cka = utils.evaluate_cka(model, retrained_model, forget_loader, device, args=args, data='Df')
-    retain_cka = utils.evaluate_cka(model, retrained_model, retain_loader, device, args=args, data='Dr')
-    val_forget_cka = utils.evaluate_cka(model, retrained_model, val_forget_loader, device, args=args, data='Dtf')
-    val_retain_cka = utils.evaluate_cka(model, retrained_model, val_retain_loader, device, args=args, data='Dtr')
+    forget_cka = utils.evaluate_cka(model, retrained_model, forget_loader, device, args=args)
+    retain_cka = utils.evaluate_cka(model, retrained_model, retain_loader, device, args=args)
+    val_forget_cka = utils.evaluate_cka(model, retrained_model, val_forget_loader, device, args=args)
+    val_retain_cka = utils.evaluate_cka(model, retrained_model, val_retain_loader, device, args=args)
     
     # Office-Home
     office_home_real_world_data_loader = utils.office_home_dataloaders(data_dir=args.office_home_dataset_path, domain="Real_World", batch_size=512, num_workers=4)
@@ -108,8 +109,8 @@ def evaluate_model(model_path, retrained_model_path, device, args):
     domainnet126_real_data_loader = domainnet126_dataloaders(batch_size=512, domain='real', data_dir=args.domainnet_dataset_path, num_workers=4)
     domainnet126_sketch_data_loader = domainnet126_dataloaders(batch_size=512, domain='sketch', data_dir=args.domainnet_dataset_path, num_workers=4)
 
-    model = utils.load_model(model_path, device).to(device)
-    retrained_model = utils.load_model(retrained_model_path, device).to(device)
+    model = utils.load_model(model_path, device, arch=args.arch).to(device)
+    retrained_model = utils.load_model(retrained_model_path, device, arch=args.arch).to(device)
     model.eval()
     retrained_model.eval()
     
@@ -160,12 +161,13 @@ def evaluate_model(model_path, retrained_model_path, device, args):
     # val_forget_dataset = val_forget_loader.dataset
     # val_retain_dataset = val_retain_loader.dataset
     # test_dataset = torch.utils.data.ConcatDataset([val_forget_dataset, val_retain_dataset])
+    # test_dataset = torch.utils.data.Subset(test_dataset, list(range(1000)))
     
     # test_loader = torch.utils.data.DataLoader(
     #     test_dataset, batch_size=args.batch_size, shuffle=False
     # )
     
-    # MIA_result = evaluation.MIA(
+    # MIA_result = evaluation.SVC_MIA(
     #     shadow_train=shadow_train_loader,
     #     shadow_test=test_loader,
     #     target_train=None,
@@ -175,10 +177,12 @@ def evaluate_model(model_path, retrained_model_path, device, args):
     
     # results.update({
     #     "MIA_forget_correctness": MIA_result['correctness'],
-    #     "MIA_forget_prob": MIA_result['prob'],
+    #     "MIA_forget_confidence": MIA_result['confidence'],
+    #     # "MIA_forget_entropy": MIA_result['entropy'],
+    #     # "MIA_forget_modified_entropy": MIA_result['m_entropy'],
+    #     # "MIA_forget_prob": MIA_result['prob'],
     # })
     
-
     return results
 
 
@@ -192,54 +196,34 @@ def main():
         run = utils.init_wandb(args, project_name="unlearning_evaluation")
         
     model_paths = [
-        # "./pretrained_model/original.pth.tar",
-        # "./pretrained_model/retrained_random100.pth.tar",
-        # "./result/GA/GA/5e-06/7/GAcheckpoint.pth.tar",
-        # "./result/RL/RL_imagenet/5e-06/6/RL_imagenetcheckpoint.pth.tar",
-        # "./result/SalUn/RL_imagenet/5e-06/9/RL_imagenetcheckpoint.pth.tar",
-        # "./result/FT/FT/0.0001/39/FTcheckpoint.pth.tar",
-        # "./result/CU/CU/0.001/79/CUcheckpoint.pth.tar",
-        # "./result/SCAR/SCAR/0.0005/26/SCARcheckpoint.pth.tar",
-        # "./result/SCRUB/SCRUB/1e-05/90/SCRUBcheckpoint.pth.tar",
-        # "./result/GAwithKD/GAwithKD/1e-05/20/GAwithKDcheckpoint.pth.tar",
-        # "./result/RKD/RKD/7e-06/14/RKDcheckpoint.pth.tar",
-        # "./result/AKD/AKD/7e-06/14/AKDcheckpoint.pth.tar",
-        # "./result/SPKD/SPKD/7e-06/14/SPKDcheckpoint.pth.tar",
-        # "./result/SPKD_retrained/SPKD_retrained/7e-06/14/SPKD_retrainedcheckpoint.pth.tar",
-        # "./pretrained_model/retrained_top100_officehome.pth.tar",
-        # "./result/GA_top100_officehome/GA/5e-06/7/GAcheckpoint.pth.tar",
-        # "./result/RL_top100_officehome/RL_imagenet/5e-06/6/RL_imagenetcheckpoint.pth.tar",
-        # "./result/SalUn_top100_officehome/RL_imagenet/5e-06/9/RL_imagenetcheckpoint.pth.tar",
-        # "./result/FT_top100_officehome/FT/0.0001/39/FTcheckpoint.pth.tar",
-        # "./result/CU_top100_officehome/CU/0.001/79/CUcheckpoint.pth.tar",
-        # "./result/SCAR_top100_officehome/SCAR/0.0005/26/SCARcheckpoint.pth.tar",
-        # "./result/SCRUB_top100_officehome/SCRUB/1e-05/90/SCRUBcheckpoint.pth.tar",
-        # "./result/GA_KD_top100_officehome/GA_KD/1e-05/20/GA_KDcheckpoint.pth.tar",
+        "./pretrained_model/original.pth.tar",
     ]
     
     
-    results = {}
+    for model_path in model_paths:
+        results = {}
+        args.model_path = model_path
 
-    eval_results = evaluate_model(args.model_path, args.retrained_model_path, device, args)
-    results.update(eval_results)
-    
-    # print results
-    print("Evaluation results:")
-    print("-" * 50)
-    for metric, value in results.items():
-        print(f"{metric}: {value:.2f}")
+        eval_results = evaluate_model(args.model_path, args.retrained_model_path, device, args)
+        results.update(eval_results)
+        
+        # print results
+        print("Evaluation results:")
+        print("-" * 50)
+        for metric, value in results.items():
+            print(f"{metric}: {value:.2f}")
 
-    try:
-        method_name = args.model_path.split('/')[-4]
-    except:
-        method_name = "original" if "original" in args.model_path else "retrained"
-        
-    if args.use_wandb:
-        wandb_results = {}
-        for metric_name, value in results.items():
-            wandb_results[f"{metric_name}"] = value
-        
-        wandb.log(wandb_results)
+        try:
+            method_name = args.model_path.split('/')[-4]
+        except:
+            method_name = "original" if "original" in args.model_path else "retrained"
+            
+        if args.use_wandb:
+            wandb_results = {}
+            for metric_name, value in results.items():
+                wandb_results[f"{metric_name}"] = value
+            
+            wandb.log(wandb_results)
 
 
 if __name__ == "__main__":
